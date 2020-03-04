c304f0e262a65f3d8086609a29be14a91977e6ab
Vince West <a3r7lzz@mmm.com>
2020-03-04 11:30:05 -0600

    initial plain coloring of powershell

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,17 @@
+Set-PSReadLineOption -Colors @{
+"ContinuationPrompt" =     [ConsoleColor]:: Black
+"Emphasis" =     [ConsoleColor]:: Black    
+"Error" =     [ConsoleColor]:: Black
+"Selection" =     [ConsoleColor]:: Black
+"Default" =     [ConsoleColor]:: Black
+"Comment" =     [ConsoleColor]:: Black
+"Keyword" =     [ConsoleColor]:: Black
+"String" =     [ConsoleColor]:: Black
+"Operator" =     [ConsoleColor]:: Black
+"Variable" =     [ConsoleColor]:: Black
+"Command" =     [ConsoleColor]:: Black
+"Parameter" =     [ConsoleColor]:: Black
+"Type" =     [ConsoleColor]:: Black
+"Number" =     [ConsoleColor]:: Black
+"Member" =     [ConsoleColor]:: Black
+}
